ccff303c8d48eba0499ef573f4ab66f1045c2aaf
realapse <realapsenomore777@gmail.com>
2024-12-06 23:24:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add click functionality to the buttons
+document.querySelector(".business").addEventListener("click", () => {
+    alert("Business sign-up coming soon!");
+});
+
+document.querySelector(".user").addEventListener("click", () => {
+    alert("User earnings dashboard coming soon!");
+});
